4c93bc915460ffdb4338ceedc4e636b5ad29a5d6
Rakshith338 <rakshithms3010@gmail.com>
2026-04-21 09:34:35 +0530

    string input & operator

--- day2.py
+++ day2.py
@@ -4,3 +4,14 @@ a = 10          #integer variable
 name = "rakshi" #string variable
 b = 5.5         #floating variable
 print(a,b,name) #built in function
+
+#input to variables
+
+a = input("enter first number: ") #input as string
+b = input ("enter second number: ")
+print (a,b)
+
+a = int(a)
+b = int(b)    
+c = a +b      # add operator
+print (c)
