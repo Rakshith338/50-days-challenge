6af345752f83f84bf621c1960120502586cfa1ae
Rakshith338 <rakshithms3010@gmail.com>
2026-04-21 09:52:44 +0530

    my simple calculator

--- day2.py
+++ day2.py
@@ -15,3 +15,18 @@ a = int(a)
 b = int(b)    
 c = a +b      # add operator
 print (c)
+
+#build my simple calculator
+#using input & operation
+#using formating strng output
+
+x = input("enter the x value:")
+y = input("enter the y value:")
+x = int(x)
+y = int(y)
+
+print (f"additon: {x+y}")
+print (f"substraction:{x-y}")
+print (f"multiplication:{x*y}")
+print (f"division: {x/y}")
+print (f"modulus: {x%y}")
