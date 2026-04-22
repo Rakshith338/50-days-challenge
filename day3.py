2bbcb814af46fab28ec9cc1dbc675640c225e908
Rakshith338 <rakshithms3010@gmail.com>
2026-04-22 09:36:56 +0530

    added largest number task

--- day3.py
+++ day3.py
@@ -52,6 +52,32 @@ enter the first number: 6
 enter the second number: 6
 enter operation (+,-,*,/):8
 invalid operation
+enter the first number: 5
+enter the second number: 5
+enter operation (+,-,*,/):*
+output:25
 """
 
 #Task#3: Largest of 3 numbers
+#20, 25, 26
+
+a = int(input("enter the first number: ")) #Take input
+b = int(input("enter the second number: ")) #Take input
+c = int(input("enter the third number: ")) #Take input
+
+if a > b and a > c:
+        print (f"{a} is largest number")
+elif b > c and b > a:
+        print (f"{b} is largest number")
+elif c > a and c > b:
+        print (f"{c} is largest number")
+else:
+        print ("invalid")
+        
+"""
+output:enter the first number: 25
+enter the second number: 26
+enter the third number: 28
+28 is largest number
+"""
+
