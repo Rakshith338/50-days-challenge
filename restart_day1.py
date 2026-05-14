3a1546e3085c3edffebc16e05241c40ca88e7452
Rakshith338 <rakshithms3010@gmail.com>
2026-05-14 12:26:31 +0530

    restart_day1.py

---
+++ restart_day1.py
@@ -0,0 +1,22 @@
+#day1 task
+
+a = 10  #variable & assign value
+b = 5
+
+print(a + b) #print statement
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+
+name = input("Enter your name: ") # input
+print("Hello " + name) #"hello" is string data type
+
+age = int(input("Enter age: ")) # integer data type
+
+user_name = "Tom & Jerry"
+
+print (user_name.lower())
+print (user_name.upper())
+
+
